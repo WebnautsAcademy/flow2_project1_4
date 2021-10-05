b7f8ca0a099882a521d1a1e897a0da9bea9bcce7
SneakyPotts <4udaksaha@gmail.com>
2021-10-05 19:00:31 +0300

    lesson #9

--- js/script.js
+++ js/script.js
@@ -1,11 +1,43 @@
-let buttonElement = document.querySelector('.button');
-let popupContainerElement = document.querySelector('.popup-container');
-let buttonClose = document.querySelector('.close');
+// let buttonElement = document.querySelector('.button');
+// let popupContainerElement = document.querySelector('.popup-container');
+// let buttonClose = document.querySelector('.close');
 
-buttonElement.addEventListener('click', () => {
-    popupContainerElement.classList.remove('popup-container-hidden');    
+// buttonElement.addEventListener('click', () => {
+//     popupContainerElement.classList.remove('popup-container-hidden');    
+// });
+
+// buttonClose.addEventListener('click', () => {
+//     popupContainerElement.classList.add('popup-container-hidden');
+// });
+
+
+let slider = document.querySelector('.slider-list');
+let slides = document.querySelectorAll('.slider-item');
+let nextButton = document.querySelector('.button-next-slide');
+let prewButton = document.querySelector('.button-prew-slide');
+
+slides.forEach((slide, i) => {
+    slide.style.left = `calc(${100 * i}% + ${160 * i}px)`;
 });
 
-buttonClose.addEventListener('click', () => {
-    popupContainerElement.classList.add('popup-container-hidden');
+let activeIndex = 0;
+let currntLeft = 0;
+
+nextButton.addEventListener('click', () => {
+    slides.forEach((slide, i) => {
+        if (slide.classList.contains('active')) {
+            activeIndex = i + 1;
+            slide.classList.remove('active');
+        }
+
+        if (slide[activeIndex]) {
+            slides[activeIndex].classList.add('active')
+        } else {
+            slide[0].classList.add('active');
+
+        }
+
+        currntLeft = 62 * activeIndex;
+        slider.style.left = `-${currntLeft}%`;
+    });
 });
